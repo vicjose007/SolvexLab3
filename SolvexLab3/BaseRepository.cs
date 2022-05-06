f11e604cab7d1494f3873be4d0d4df52cf743e26
vicjose007 <vicjose007@gmail.com>
2022-05-06 09:07:17 -0400

    Add project files.

---
+++ SolvexLab3/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolvexLab3
+{
+    public class BaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        public List<TEntity> People { get; set; }
+
+        private int PersonCounter = 1;
+
+        public TEntity Add(TEntity entity)
+        {
+
+            entity.Id = PersonCounter;
+            People.Add(entity);
+
+            PersonCounter++;
+            return entity;
+        }
+
+        public TEntity Update(int id, TEntity newPerson)
+        {
+            if (id != newPerson.Id) throw new Exception("id and person id does not exist");
+            People.Add(newPerson);
+            return newPerson;
+        }
+        public TEntity Delete(int personid)
+        {
+            var exist = People.Any(entity => entity.Id == entity.Id);
+
+            var entity = People.Where(entity => entity.Id == personid).FirstOrDefault();
+
+            People.Remove(entity);
+
+            if (entity is null) throw new Exception("Person does not exist");
+
+            return entity;
+
+        }
+
+
+
+    }
+}
---
+++ SolvexLab3/Program.cs
@@ -0,0 +1,59 @@
+﻿public enum Gender
+{
+    Male = 1,
+    Female = 2,
+    Dinosaurio = 3,
+    Helicoptero = 4,
+    Tractor = 5,
+}
+
+public class BaseEntity
+{ 
+    public int Id { get; set; } 
+
+}
+
+public class Person : BaseEntity 
+{
+    public string? Name { get; set; }
+    public Gender Gender { get; set; }
+    public int Age { get; set; }
+
+    public Person(int Id,string Name, Gender Gender, int Age)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Gender = Gender;
+        this.Age = Age;
+
+    } 
+    public void createPerson()
+    {
+        Person Person1 = new Person(Id, Name, Gender, Age);
+
+
+        Console.WriteLine("Ingrese Id");
+        Person1.Id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese Nombre");
+        Person1.Name = Console.ReadLine();
+
+        Console.WriteLine("Ingrese edad");
+        Person1.Age = int.Parse(Console.ReadLine());
+
+
+    }
+    public void Main(string[] args)
+    {
+        createPerson();
+        
+    }
+
+
+}
+
+
+
+
+
+
